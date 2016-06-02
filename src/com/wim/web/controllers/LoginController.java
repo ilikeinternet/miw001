ed12fb9b3cd71e2fa473129f2fc7388d27d39262
jtli <JTLi@JT-Li>
2016-06-02 10:24:32 +0800

    updated

---
+++ src/com/wim/web/controllers/LoginController.java
@@ -0,0 +1,17 @@
+/**
+ * MIW System web controller
+ */
+package com.wim.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * @author JTLi; 16/06/01
+ * System login controller
+ */
+@Controller
+@SessionAttributes()
+public class LoginController {
+
+}
--- src/com/wim/web/controllers/UserController.java
+++ src/com/wim/web/controllers/UserController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@RequestMapping("/add")
-	public void add(String n, String p){
-		System.out.println("add---");
-		System.out.println(n+"---add---"+p);
-		
-	}
+   @RequestMapping("/add")
+   public void add(String n, String p) {
+      System.out.println("add---");
+      System.out.println(n + "---add---" + p);
+
+   }
 }
